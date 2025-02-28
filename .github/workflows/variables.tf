a56fbb339d531909f0dde9e243b1226057ffbc17
@nesto26 <nestork093@gmail.com>
2025-02-28 12:56:20 -0600

    Add EKS deployment workflow8

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-2"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  default     = "my-eks-cluster"
+}
--- my-eks-project/main.tf
+++ my-eks-project/main.tf
@@ -1,46 +1,46 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.25"
-
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size = 2
-      min_size     = 1
-      max_size     = 3
-      instance_types = ["t2.micro"]
-    }
-  }
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
-
-  name = "${var.cluster_name}-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-# output "kubeconfig" {
-#   value = module.eks.kubeconfig
+# provider "aws" {
+#   region = var.aws_region
 # }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 19.0"
+
+#   cluster_name    = var.cluster_name
+#   cluster_version = "1.25"
+
+#   vpc_id          = module.vpc.vpc_id
+#   subnet_ids      = module.vpc.private_subnets
+
+#   eks_managed_node_groups = {
+#     default = {
+#       desired_size = 2
+#       min_size     = 1
+#       max_size     = 3
+#       instance_types = ["t2.micro"]
+#     }
+#   }
+# }
+
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "~> 4.0"
+
+#   name = "${var.cluster_name}-vpc"
+#   cidr = "10.0.0.0/16"
+
+#   azs             = ["us-east-1a", "us-east-1b"]
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+#   enable_nat_gateway = true
+#   single_nat_gateway = true
+# }
+
+# output "cluster_name" {
+#   value = module.eks.cluster_name
+# }
+
+# # output "kubeconfig" {
+# #   value = module.eks.kubeconfig
+# # }
