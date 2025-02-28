8f22f9f78103f58a6a4e0d8f30e40557b3281c99
@nesto26 <nestork093@gmail.com>
2025-02-28 13:08:27 -0600

    Add EKS deployment workflow9

--- my-eks-project/variables.tf
+++ my-eks-project/variables.tf
@@ -1,9 +1,9 @@
-# variable "aws_region" {
-#   description = "AWS region"
-#   default     = "us-east-2"
-# }
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-2"
+}
 
-# variable "cluster_name" {
-#   description = "Name of the EKS cluster"
-#   default     = "my-eks-cluster"
-# }
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  default     = "my-eks-cluster"
+}
