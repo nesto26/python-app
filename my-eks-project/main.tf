69d3f785e0d8855ca0f56cb60c5a40e55dddc5d2
@nesto26 <nestork093@gmail.com>
2025-03-01 09:48:53 -0600

    Add EKS deployment workflow13

--- my-eks-project/main.tf
+++ my-eks-project/main.tf
@@ -29,7 +29,7 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["us-east-2a", "us-east-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
